9d94ba3ea884d243c573e48090546f529503b55d
iketaurus <iketaurus@gmail.com>
2019-05-28 19:18:27 +0000

    Add hello

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'static_pages/jobs'
   get 'static_pages/living'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-application#hello 
+root 'application#hello'
 
 end
