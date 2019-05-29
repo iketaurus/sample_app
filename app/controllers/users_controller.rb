4fe2f25f3c400f48036dc0ad2932f5b9941b9ab5
iketaurus <iketaurus@gmail.com>
2019-05-29 19:21:50 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
-  def show
-      @user=user.find (params[:id])
-      
+  def new
+    @user = User.new
+  end
+  
+    def show
+      @user=user.find(params[:id])
+    end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to '/'
+    end 
+  end 
+  private
+  def user_params
+    params.require(:user).permit(:name, :school)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   get 'static_pages/jobs'
   get 'static_pages/living'
   get 'static_pages/fun'
-  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'static_pages#homes'
+
+  resources :users
 
 end
