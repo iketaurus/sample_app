65e2c70d52c5fd6e29524e1ce12f02db40112c16
iketaurus <iketaurus@gmail.com>
2019-05-29 19:42:28 +0000

    Use SSL and the Puma webservr in production

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "get signup_path" do
-    get users_new_url
+    get signup_path
     assert_response :success
   end
 
